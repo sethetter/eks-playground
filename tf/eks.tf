cd584dcf0812d75bc0fc85dcb27f83e7ce7d95c8
Seth Etter <mail@sethetter.com>
2022-02-15 17:00:34 -0600

    use the aws vpc module

--- tf/eks.tf
+++ tf/eks.tf
@@ -1,19 +1,3 @@
-resource "aws_eks_cluster" "main" {
-  name = "playground"
-  role_arn = aws_iam_role.eks_cluster.arn
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.public.id,
-      aws_subnet.private_a.id,
-      aws_subnet.private_b.id
-    ]
-  }
-  depends_on = [
-    aws_iam_role.eks_cluster,
-    aws_iam_role_policy_attachment.eks_cluster-AmazonEKSClusterPolicy
-  ]
-}
-
 resource "aws_ecr_repository" "eks_playground" {
   name = "eks-playground"
   image_tag_mutability = "MUTABLE"
@@ -22,11 +6,26 @@ resource "aws_ecr_repository" "eks_playground" {
   }
 }
 
+resource "aws_eks_cluster" "main" {
+  name = "eks-playground"
+  role_arn = aws_iam_role.eks_cluster.arn
+  vpc_config {
+    subnet_ids = concat(
+      module.vpc.private_subnets,
+      module.vpc.public_subnets
+    )
+  }
+  depends_on = [
+    aws_iam_role.eks_cluster,
+    aws_iam_role_policy_attachment.eks_cluster-AmazonEKSClusterPolicy
+  ]
+}
+
 resource "aws_eks_fargate_profile" "main" {
   cluster_name           = aws_eks_cluster.main.name
   fargate_profile_name   = "main"
   pod_execution_role_arn = aws_iam_role.eks_playground_pods.arn
-  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+  subnet_ids = module.vpc.private_subnets
 
   selector {
     namespace = "default"
@@ -37,7 +36,7 @@ resource "aws_eks_fargate_profile" "system" {
   cluster_name           = aws_eks_cluster.main.name
   fargate_profile_name   = "system"
   pod_execution_role_arn = aws_iam_role.eks_playground_pods.arn
-  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+  subnet_ids = module.vpc.private_subnets
 
   selector {
     namespace = "kube-system"
